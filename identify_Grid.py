f4a42834914d9009308a3830576a59caafcec1d3
6200279 <45390835+6200279@users.noreply.github.com>
2021-11-03 17:33:39 +0100

    Update identify_Grid.py

--- identify_Grid.py
+++ identify_Grid.py
@@ -1,38 +1,3 @@
-import cv2 # use pip install open cv
-import numpy as np
-
-"""
-**** uncomment cv2.imshow to see resulting image !!!!
-
-cropped image coordinates with paper edge img[125:675, 200:975] (format [x:x+h][y:y+w])
-cropped image coordinates without paper edge img[130:660, 220:960]
-cropped image coordinates without gr code img[250:660, 220:760] 
-"""
-
-img = cv2.imread('webcam2.jpeg')  # image for now will be be made into a video stream
-
-print(img.shape)  # Print image shape
-
-kernelsize =5
-
-blurr = cv2.medianBlur(img, kernelsize)
-
-#cv2.imshow("orignal", img)
-#cv2.imshow("blurr", blurr)
-
-
-""" crop gameboard out of picture"""
-cropped_image = img[250:660, 220:760]   # cropped image
-cropped_blurr =blurr[250:660, 220:760] # blurred cropped image
-cv2.imshow("cropped", cropped_image)
-
- # convert cropped image to gray scale
-cropped_gray = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
-#cv2.imshow("cropped gray", cropped_gray)
-
-cropped_gray_blurry = cv2.cvtColor(cropped_blurr, cv2.COLOR_BGR2GRAY) # blurred cropped grayscaled
-
-
 
 
 
@@ -40,6 +5,9 @@ cropped_gray_blurry = cv2.cvtColor(cropped_blurr, cv2.COLOR_BGR2GRAY) # blurred 
 thresh = cv2.adaptiveThreshold(cropped_gray_blurry ,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
             cv2.THRESH_BINARY,11,6)
 cv2.imshow("thres", thresh)
+
+inverted_threshold = cv2.bitwise_not(thresh)
+cv2.imshow("invert thres",inverted_threshold)
 
 
 """
@@ -61,28 +29,29 @@ cv2.imshow('threshold_3',thresh_3)
 edges = cv2.Canny(thresh, 50, 200)
 cv2.imshow("edges", edges)
 # Detect points that form a line
-lines = cv2.HoughLinesP(edges, 1, np.pi/180, 80, minLineLength=10, maxLineGap=500)
+lines = cv2.HoughLinesP(inverted_threshold, 1, np.pi/180, 80, minLineLength=10, maxLineGap=50)
 # Draw lines on the image
 for line in lines:
     x1, y1, x2, y2 = line[0]
-    cv2.line(cropped_gray, (x1, y1), (x2, y2), (255, 0, 0), 3)
+    cv2.line(inverted_threshold, (x1, y1), (x2, y2), (255, 0, 0), 3)
 
 # param1 = upperbound on threshold of canny, param2 is lowerbound on threshold
-circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 1, img.shape[0]/6, param1=200, param2=25, minRadius=15, maxRadius=30)
+circles = cv2.HoughCircles(inverted_threshold, cv2.HOUGH_GRADIENT, 1, img.shape[0]/6, param1=200, param2=25, minRadius=15, maxRadius=30)
 # Draw detected circles
 if circles is not None:
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
         # Draw outer circle
-        cv2.circle(cropped_gray, (i[0], i[1]), i[2], (0, 255, 0), 2)
+        cv2.circle(inverted_threshold, (i[0], i[1]), i[2], (255, 0, 0), 2)
         # Draw inner circle
-        cv2.circle(cropped_gray, (i[0], i[1]), 2, (0, 0, 255), 3)
+        cv2.circle(inverted_threshold, (i[0], i[1]), 2, (255, 0, 0), 3)
 
 
 # result
-cv2.imshow("Result Image", cropped_gray)
 
-
+cv2.imshow("Result Image", inverted_threshold)
+#cv2.imwrite("edges.png",edges)
+cv2.imwrite("invertedthreshold.png", inverted_threshold)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
