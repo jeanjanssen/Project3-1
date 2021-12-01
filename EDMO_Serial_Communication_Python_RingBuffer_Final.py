dddfba3460f9e87d6d52b6da2c3df6b74383c33d
jeanjanssen <jean.janssen2000@gmail.com>
2021-12-01 14:45:15 +0100

    Reformatting of output_list

--- EDMO_Serial_Communication_Python_RingBuffer_Final.py
+++ EDMO_Serial_Communication_Python_RingBuffer_Final.py
@@ -1,43 +1,43 @@
-import serial 
+import serial
 import numpy as np
 from timeit import default_timer as timer
 
 ######## INIT serial communication variables ################################
 import IK
 
-ser = serial.Serial('COM10', 57600)     # select com-port and the serial com baud rate
-ser.flushInput()    # empty the serial buffer
-ser_input = []      # emtpy the serial input array
-current_time = 0 
+ser = serial.Serial('COM10', 57600)  # select com-port and the serial com baud rate
+ser.flushInput()  # empty the serial buffer
+ser_input = []  # emtpy the serial input array
+current_time = 0
 prev_time = 0
-interval = 0.5      # serial read update time
-
+interval = 0.5  # serial read update time
 
 # Example commands to drive the motors
-#commandString = '0,0,25,0,0,45,1000,0,0,5000\n'     # data string to send to arduino
-#commandString1 = '1,2,5,0,2,10,1000,2,30,2000\n'    # data string to send to arduino
-#commandString2 = '2,3,45,0,3,-45,1000,3,0,5000\n'   # data string to send to arduino
-#commandString3 = '3,2,10,0,2,0,1000,2,-10,5000\n'   # data string to send to arduino
-#commandString4 = '4,3,45,0,3,-45,1000,3,0,5000\n'   # data string to send to arduino
-commandString5 = 'S,1,0,0,2,0,2000\n'               # S,motor,angle,delay,motor,angle,delay
-commandString6 = 'A,0,0,0,1,45,0,2,45,0,3,0,0\n'
+# commandString = '0,0,25,0,0,45,1000,0,0,5000\n'     # data string to send to arduino
+# commandString1 = '1,2,5,0,2,10,1000,2,30,2000\n'    # data string to send to arduino
+# commandString2 = '2,3,45,0,3,-45,1000,3,0,5000\n'   # data string to send to arduino
+# commandString3 = '3,2,10,0,2,0,1000,2,-10,5000\n'   # data string to send to arduino
+# commandString4 = '4,3,45,0,3,-45,1000,3,0,5000\n'   # data string to send to arduino
+commandString5 = 'S,1,0,0,2,0,2000\n'  # S,motor,angle,delay,motor,angle,delay
+commandString6 = 'A,0,-30,0,1,0,0,2,0,0,3,0,0\n'
 
 
 ############################# SEND DATA ##################################
 # function sending data from the PC to the Arduino
 def sendData():
     print("Sending command to arduino:")
-    #print(commandString)
-    #ser.write(commandString.encode())
-    #ser.write(commandString1.encode())
-    #ser.write(commandString2.encode())
-    #ser.write(commandString3.encode())
-    #ser.write(commandString4.encode())
-    #ser.write(commandString5.encode())
+    # print(commandString)
+    # ser.write(commandString.encode())
+    # ser.write(commandString1.encode())
+    # ser.write(commandString2.encode())
+    # ser.write(commandString3.encode())
+    # ser.write(commandString4.encode())
+    # ser.write(commandString5.encode())
     output = IK.getcoords(30, 10, 22.1, 69)
-    ser.write(output.encode())
-    # for x in output:
-    #     ser.write(x.encode())
+    ser.write(commandString6.encode())
+    for x in output:
+        ser.write(x.encode())
+    # ser.write(output.encode())
 
 
 ############################# RECEIVE DATA ##################################
@@ -47,10 +47,10 @@ def recData():
     # if data on the serial interface, then read it
     if ser.in_waiting > 0:
         print("Reading serial input")
-        ser_input = ser.readline()[:-2]         # the last bit gets rid of the new-line chars
-        ser_input = ser_input.decode('ascii')   # decode input
+        ser_input = ser.readline()[:-2]  # the last bit gets rid of the new-line chars
+        ser_input = ser_input.decode('ascii')  # decode input
         print("Data Received from Arduino:")
-        print(ser_input)                        # print received data
+        print(ser_input)  # print received data
 
 
 ############################# MAIN ##################################
@@ -60,6 +60,6 @@ sendData()  # send drive commands once
 while True:
     current_time = timer()  # update current time
     # if interval is reached, check if there is a new serial input
-    if current_time-prev_time >= interval:
+    if current_time - prev_time >= interval:
         prev_time = current_time
         recData()
--- IK.py
+++ IK.py
@@ -41,7 +41,7 @@ def getcoords(px, py, pz, phi):
 
     phi = rad2deg(phi)
     theta_1 = rad2deg(theta_1)
-    print("Theta_1 =", theta_1)
+    # print("Theta_1 =", theta_1)
     theta_1 = min(max(-45, theta_1), 45)
     theta_2 = rad2deg(theta_2)
     theta_2 = min(max(-45, theta_2), 45)
@@ -49,7 +49,6 @@ def getcoords(px, py, pz, phi):
     theta_3 = rad2deg(theta_3)
     theta_3 = min(max(-45, theta_3), 45)
     theta_4 = phi - theta_2 - theta_3
-    theta_4 = theta_4
     theta_4 = min(max(-90, theta_4), 90)
 
     # For every theta, getting the moves in a list with at most 5 degrees at a time,
@@ -58,56 +57,53 @@ def getcoords(px, py, pz, phi):
     t1 = 0
     for x in range(0, abs(math.floor(theta_1 / 5))):
         if theta_1 > 0:
-            output_list.append('0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 3, {}, 0\n'.format(5 * (t1 + 1)))
+            output_list.append('A,0,0,0,1,0,0,2,0,0,3,{},0\n'.format(5 * (t1 + 1)))
             t1 += 1
         elif theta_1 < 0:
-            output_list.append('0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 3, {}, 0\n'.format(-5 * t1))
+            output_list.append('A,0,0,0,1,0,0,2,0,0,3,{},0\n'.format(-5 * t1))
             t1 += 1
     if theta_1 - 5 * t1 != 0:
-        output_list.append('0, 0, 0, 0, 1, 0, 0, 2, 0, 0, 3, {}, 0\n'.format(theta_1))
+        output_list.append('A,0,0,0,1,0,0,2,0,0,3,{},0\n'.format(theta_1))
 
     t2 = 0
     for x in range(0, abs(math.floor(theta_2 / 5))):
         if theta_2 > 0:
-            output_list.append('0, 0, 0, 0, 1, 0, 0, 2, {}, 0, 3, {}, 0\n'.format(5 * (t2 + 1), theta_1))
+            output_list.append('A,0,0,0,1,0,0,2,{},0,3,{},0\n'.format(5 * (t2 + 1), theta_1))
             t2 += 1
         elif theta_2 < 0:
-            output_list.append('0, 0, 0, 0, 1, 0, 0, 2, {}, 0, 3, {}, 0\n'.format(-5 * t2, theta_1))
+            output_list.append('A,0,0,0,1,0,0,2,{},0,3,{},0\n'.format(-5 * t2, theta_1))
             t2 += 1
     if theta_2 - 5 * t2 != 0:
-        output_list.append('0, 0, 0, 0, 1, 0, 0, 2, {}, 0, 3, {}, 0\n'.format(theta_2, theta_1))
+        output_list.append('A,0,0,0,1,0,0,2,{},0,3,{},0\n'.format(theta_2, theta_1))
 
     t3 = 0
     for x in range(0, abs(math.floor(theta_3 / 5))):
         if theta_3 > 0:
-            output_list.append('0, 0, 0, 0, 1, {}, 0, 2, {}, 0, 3, {}, 0\n'.format(5 * (t3 + 1), theta_2, theta_1))
+            output_list.append('A,0,0,0,1,{},0,2,{},0,3,{},0\n'.format(5 * (t3 + 1), theta_2, theta_1))
             t3 += 1
         elif theta_3 < 0:
-            output_list.append('0, 0, 0, 0, 1, {}, 0, 2, {}, 0, 3, {}, 0\n'.format(-5 * t3, theta_2, theta_1))
+            output_list.append('A,0,0,0,1,{},0,2,{},0,3,{},0\n'.format(-5 * t3, theta_2, theta_1))
             t3 += 1
     if theta_3 - 5 * t3 != 0:
-        output_list.append('0, 0, 0, 0, 1, {}, 0, 2, {}, 0, 3, {}, 0\n'.format(theta_3, theta_2, theta_1))
+        output_list.append('A,0,0,0,1,{},0,2,{},0,3,{},0\n'.format(theta_3, theta_2, theta_1))
 
     t4 = 0
     for x in range(0, abs(math.floor(theta_4 / 5))):
         if theta_4 > 0:
             output_list.append(
-                '0, 0, {}, 0, 1, {}, 0, 2, {}, 0, 3, {}, 0\n'.format(5 * (t4 + 1), theta_3, theta_2, theta_1))
+                'A,0,{},0,1,{},0,2,{},0,3,{},0\n'.format(5 * (t4 + 1), theta_3, theta_2, theta_1))
             t4 += 1
         elif theta_4 < 0:
             output_list.append(
-                '0, 0, {}, 0, 1, {}, 0, 2, {}, 0, 3, {}, 0\n'.format(-5 * t4, theta_3, theta_2, theta_1))
+                'A,0,{},0,1,{},0,2,{},0,3,{},0\n'.format(-5 * t4, theta_3, theta_2, theta_1))
             t4 += 1
     if theta_4 - 5 * t4 != 0:
         output_list.append(
-            '0, 0, {}, 0, 1, {}, 0, 2, {}, 0, 3, {}, 0\n'.format(theta_4, theta_3, theta_2, theta_1))
+            'A,0,{},0,1,{},0,2,{},0,3,{},0\n'.format(theta_4, theta_3, theta_2, theta_1))
 
     output = 'A,0,{},0,1,{},0,2,{},0,3,{},0\n'.format(theta_4, theta_3, theta_2, theta_1)
 
-    print(output)
+    # print(output)
     for x in output_list:
         print(x)
-    return output
-
-
-getcoords(27.3, 31.7, 29.2, 32.4)
+    return output_list
