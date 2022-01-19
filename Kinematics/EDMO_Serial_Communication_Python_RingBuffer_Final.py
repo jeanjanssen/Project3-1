10c3735cf4d7597da968c9142c76efc894cf4e40
Matthijs1601 <56159553+Matthijs1601@users.noreply.github.com>
2022-01-19 22:19:25 +0100

    Cleaned EDMO_Serial_etc. and added code for demonstrating

--- Kinematics/EDMO_Serial_Communication_Python_RingBuffer_Final.py
+++ Kinematics/EDMO_Serial_Communication_Python_RingBuffer_Final.py
@@ -1,120 +1,41 @@
 import serial
-from timeit import default_timer as timer
-from time import sleep
 
-######## INIT serial communication variables ################################
 from Kinematics import IK
 from Kinematics import FK
 
-print("initializing serial")
+# Initialize serial communication variables
 ser = serial.Serial('COM10', 57600)  # select com-port and the serial com baud rate
 ser.flushInput()  # empty the serial buffer
 ser_input = []  # emtpy the serial input array
 current_time = 0
 prev_time = 0
 interval = 0.5  # serial read update time
-print("initializing done")
+
 # Example commands to drive the motors
-# commandString = '0,0,25,0,0,45,1000,0,0,5000\n'     # data string to send to arduino
-# commandString1 = '1,2,5,0,2,10,1000,2,30,2000\n'    # data string to send to arduino
-# commandString2 = '2,3,45,0,3,-45,1000,3,0,5000\n'   # data string to send to arduino
-# commandString3 = '3,2,10,0,2,0,1000,2,-10,5000\n'   # data string to send to arduino
-# commandString4 = '4,3,45,0,3,-45,1000,3,0,5000\n'   # data string to send to arduino
-
-# Stop command since ID = S
-commandString5 = 'S,0,0,0,1,0,0,2,0,0,3,0,2000\n'  # S,motor,angle,delay,motor,angle,delay
+commandStringStop = 'S,0,0,0,1,0,0,2,0,0,3,0,2000\n'  # Stop command since ID = S
 # CommandString to move the arm up straight
-commandString6 = 'A,0,0,100,1,-45,100,2,-25,100,3,0,2000\n'
+commandStringActivateMotors = 'A,0,0,100,1,-45,100,2,-25,100,3,0,2000\n'
 
 
-############################# SEND DATA ##################################
-# function sending data from the PC to the Arduino
+# Function sending data from the PC to the Arduino
 def sendData(commandString):
-    print("Sending command to arduino:")
-    print(commandString, end="")
+    print("Sending command to arduino:", commandString)
     ser.write(commandString.encode())
 
-    # Move the arm to the 0 position
-    # ser.write(commandString6.encode())
-
-    ##### TESTING INVERSE KINEMATICS #####
-    # # Formatting is IK.getcoords(x, y, z, theta_3)
-    # x = 15
-    # y = 20
-    # z = 1
-    # theta_3 = 95 if y < 25 else 50
-    # theta1, theta2, theta3, theta4 = IK.getcoords(x, y, z, theta_3)
-    # print("Calculating position given the angles of the inverse kinematics...")
-    # print(FK.calc_position(theta1, theta2, theta3, theta4))
-    #
-    # # Applying offset for the motors
-    # theta2, theta3 = IK.applyOffset(theta2, theta3)
-    #
-    # Getting the commandStrings
-    # output = IK.make_list(theta1, theta2, theta3, theta4)
-
-    ##### TESTING WITH DRAWING
-    # y_start = 22.5
-    # theta_3 = 95 if y_start < 25 else 50
-    # output = IK.drawLine(-15, 19, -15, 16, theta_3)
-
-    # y = 30
-    # theta_3 = 95 if y <= 25 else 50
-    # output = IK.drawPlus(8.5, y, 11.5, y, theta_3)
-    # output = IK.drawBox(8.5, y, 11.5, y, theta_3)
-    # output.extend(IK.drawLine(0, 25, 5, 25, theta_3))
-
-    # output = IK.drawLine(-2.5, 17.5, -2.5, 32.5, theta_3)
-    # output.extend(IK.drawLine(2.5, 17.5, 2.5, 32.5, theta_3))
-    # output.extend(IK.drawLine(-7.5, 22.5, 7.5, 22.5, theta_3))
-    # output.extend(IK.drawLine(-7.5, 27.5, 7.5, 27.5, theta_3))
-
-    # Sending the commandStrings
-    # for x in output:
-    #     print("sending", x, end="")
-    #     ser.write(x.encode())
-
-    # Second run to see if it moves to the next position correctly as well
-    second_run = False
-    if second_run:
-        y_start = 30
-        theta_3 = 95 if y_start < 25 else 50
-        theta1, theta2, theta3, theta4 = IK.getcoords(-10, y_start, 1, theta_3)
-        print("Calculating position given the angles of the inverse kinematics...")
-        print(FK.calc_position(theta1, theta2, theta3, theta4))
-
-        # Applying offset for the motors
-        theta2, theta3 = IK.applyOffset(theta2, theta3)
-
-        # Getting the commandStrings
-        output = IK.make_list(theta1, theta2, theta3, theta4)
+    # Demonstration of drawing:
+    demonstrate = False
+    if demonstrate:
+        y = 22.5
+        theta3 = 95 if y < 25 else 50
+        # output_list = IK.drawLine(2.5, y, 2.5, 27.5, theta3, returnCommandString=True, shortStrings=False)
+        output_list = IK.drawPlus(2.5, y, 2.5, y-5, theta3, shortStrings=False)
+        # output_list = IK.drawBox(0, y, 5, y, theta3, shortStrings=False)
 
         # Sending the commandStrings
-        for x in output:
-            print("sending", x, end="")
-            ser.write(x.encode())
+        for output in output_list:
+            print("sending", output, end="")
+            ser.write(output.encode())
 
 
-############################# RECEIVE DATA ##################################
-# Function checking if data has been received and if yes reading it
-def recData():
-    print("Checking for serial input")
-    # if data on the serial interface, then read it
-    if ser.in_waiting > 0:
-        print("Reading serial input")
-        ser_input = ser.readline()[:-2]  # the last bit gets rid of the new-line chars
-        ser_input = ser_input.decode('ascii')  # decode input
-        print("Data Received from Arduino:")
-        print(ser_input)  # print received data
-
-
-############################# MAIN ##################################
-sendData(commandString6)  # send drive commands once
-
-# continuously check and read serial input
-# while True:
-#     current_time = timer()  # update current time
-#     # if interval is reached, check if there is a new serial input
-#     if current_time - prev_time >= interval:
-#         prev_time = current_time
-#         # recData()
+# Send commandString to power all the motors in a vertical position
+sendData(commandStringActivateMotors)
